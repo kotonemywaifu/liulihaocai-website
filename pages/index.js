3f9d3cab967b0e6ce1443913c893968ba576369c
liulihaocai <liulihaocaiqwq@gmail.com>
2022-04-30 12:55:29 +0800

    add noreferrer

--- pages/index.js
+++ pages/index.js
@@ -19,11 +19,11 @@ export default function Home() {
           <div className="idx-text">Liulihaocai, <br /> <span className="type-text"><Typewriter texts={["Java/Kotlin developer.", "on progressing front-end developer.", "gamer."]} cursor="|" /></span></div>
           <div className="idx-center-line" />
           <div className="idx-social">
-            <a href="https://github.com/liulihaocai" target="_blank"><FontAwesomeIcon className="idx-icon" icon={faGithub} /></a>
-            <a href="https://www.youtube.com/channel/UCVwMDCM2oUvzQthG8EfDpHQ" target="_blank"><FontAwesomeIcon className="idx-icon" icon={faYoutube} /></a>
-            <a href="https://steamcommunity.com/profiles/76561199179237723/" target="_blank"><FontAwesomeIcon className="idx-icon" icon={faSteam} /></a>
-            <a href="https://stackoverflow.com/users/16676567/liulichan" target="_blank"><FontAwesomeIcon className="idx-icon" icon={faStackOverflow} /></a>
-            <a href="https://leetcode.com/liulihaocaiqwq/" target="_blank"><FontAwesomeIcon className="idx-icon" icon={faCode} /></a>
+            <a href="https://github.com/liulihaocai" target="_blank" rel="noreferrer"><FontAwesomeIcon className="idx-icon" icon={faGithub} /></a>
+            <a href="https://www.youtube.com/channel/UCVwMDCM2oUvzQthG8EfDpHQ" target="_blank" rel="noreferrer"><FontAwesomeIcon className="idx-icon" icon={faYoutube} /></a>
+            <a href="https://steamcommunity.com/profiles/76561199179237723/" target="_blank" rel="noreferrer"><FontAwesomeIcon className="idx-icon" icon={faSteam} /></a>
+            <a href="https://stackoverflow.com/users/16676567/liulichan" target="_blank" rel="noreferrer"><FontAwesomeIcon className="idx-icon" icon={faStackOverflow} /></a>
+            <a href="https://leetcode.com/liulihaocaiqwq/" target="_blank" rel="noreferrer"><FontAwesomeIcon className="idx-icon" icon={faCode} /></a>
           </div>
         </div>
       </div>
